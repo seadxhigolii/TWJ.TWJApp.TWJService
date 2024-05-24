fd37439e31c63188f602e0584d8e9b351985c0a9
Sead Xhigoli <sead.xhigoli@outlook.com>
2024-05-25 01:17:20 +0200

    TWJ-7 Costumized the bullet points for each product advertisement

--- src/TWJ.TWJApp.TWJService.Application/Interfaces/IOpenAiService.cs
+++ src/TWJ.TWJApp.TWJService.Application/Interfaces/IOpenAiService.cs
@@ -1,34 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 using TWJ.TWJApp.TWJService.Application.Dto.Models;
-using TWJ.TWJApp.TWJService.Application.Services.BlogPost.Commands.Generate;
 using TWJ.TWJApp.TWJService.Domain.Enums;
 
 namespace TWJ.TWJApp.TWJService.Application.Interfaces
 {
     public interface IOpenAiService
     {
-        //Task<string> GenerateBlogPostAsync(GenerateBlogPostCommand command, CancellationToken cancellationToken);
-
         Task<string> GenerateTagsAsync(string content, Guid blogPostId, CancellationToken cancellationToken);
         Task<BlogPostResponse> GenerateBlogPostAsync(BlogPostType postType, CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateSEOFocusedBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateMythBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateWeightLossBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateNutritionalAdviceBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateExerciseGuidesBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateRecipesAndMealPlansBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateNumberedBlogPostTitleAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateBrightsideBlogPostTitleAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateMindsetAndMotivationBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateLatestNewsBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GenerateHealthyLifestyleBlogPostAsync(CancellationToken cancellationToken);
-        Task<BlogPostResponse> GeneratePersonalStoryBlogPostAsync(CancellationToken cancellationToken);
 
     }
 }
